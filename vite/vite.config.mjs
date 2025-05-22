d69ebaca7e01ec3d9fe88c5b33c964527d622379
Asfand Yar <asfi.official@gmail.com>
2025-05-22 12:36:42 +0500

    thema react

--- vite/vite.config.mjs
+++ vite/vite.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import jsconfigPaths from 'vite-jsconfig-paths';
 
+
 export default defineConfig(({ mode }) => {
   // depending on your application, base can also be "/"
   const env = loadEnv(mode, process.cwd(), '');
